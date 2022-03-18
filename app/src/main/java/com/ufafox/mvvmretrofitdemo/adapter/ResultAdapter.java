377b6bd9bfcfcc2efa76115d4741013397c69c64
eugen <eugeny.garifullin@gmail.com>
2022-03-18 17:44:46 +0300

    paging library part2

--- app/src/main/java/com/ufafox/mvvmretrofitdemo/adapter/ResultAdapter.java
+++ app/src/main/java/com/ufafox/mvvmretrofitdemo/adapter/ResultAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.paging.PagedListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -21,13 +22,14 @@ import com.ufafox.mvvmretrofitdemo.view.MovieDetailsActivity;
 
 import java.util.ArrayList;
 
-public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultViewHolder>{
+public class ResultAdapter extends PagedListAdapter<Result, ResultAdapter.ResultViewHolder> {
     private Context context;
-    private ArrayList<Result> results;
+//    private ArrayList<Result> results;
 
-    public ResultAdapter(Context context, ArrayList<Result> results) {
+    public ResultAdapter(Context context) {
+        super(Result.CALLBACK);
         this.context = context;
-        this.results = results;
+//        this.results = results;
     }
 
     @NonNull
@@ -43,17 +45,17 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultView
     @Override
     public void onBindViewHolder(@NonNull ResultViewHolder holder, int position) {
 
-        Result result = results.get(position);
+        Result result = getItem(position);
 
         holder.resultListItemBinding.setResult(result);
 
 
     }
 
-    @Override
-    public int getItemCount() {
-        return results.size();
-    }
+//    @Override
+//    public int getItemCount() {
+//        return results.size();
+//    }
 
     public class ResultViewHolder extends RecyclerView.ViewHolder{
 
@@ -71,7 +73,7 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.ResultView
                     int position = getAdapterPosition();
                     if (position!= RecyclerView.NO_POSITION){
 
-                        Result result = results.get(position);
+                        Result result = getItem(position);
                         Intent intent = new Intent(context, MovieDetailsActivity.class);
                         intent.putExtra("movieData", result);
                         context.startActivity(intent);
--- app/src/main/java/com/ufafox/mvvmretrofitdemo/viewmodel/MainActivityViewModel.java
+++ app/src/main/java/com/ufafox/mvvmretrofitdemo/viewmodel/MainActivityViewModel.java
@@ -5,19 +5,49 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.paging.LivePagedList;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
 
+import com.ufafox.mvvmretrofitdemo.model.MovieDataSource;
+import com.ufafox.mvvmretrofitdemo.model.MovieDataSourceFactory;
 import com.ufafox.mvvmretrofitdemo.model.MovieRepository;
 import com.ufafox.mvvmretrofitdemo.model.Result;
+import com.ufafox.mvvmretrofitdemo.service.MovieApiService;
+import com.ufafox.mvvmretrofitdemo.service.RetrofitInstance;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MainActivityViewModel extends AndroidViewModel {
 
     private MovieRepository movieRepository;
+    private LiveData<MovieDataSource> movieDataSourceLiveData;
+    private Executor executor;
+    private LiveData<PagedList<Result>> pagedListLiveData;
 
     public MainActivityViewModel(@NonNull Application application) {
         super(application);
         movieRepository = new MovieRepository(application);
+
+        MovieApiService movieApiService = RetrofitInstance.getService();
+        MovieDataSourceFactory movieDataSourceFactory = new MovieDataSourceFactory(application,
+                                                                movieApiService);
+
+        movieDataSourceLiveData = movieDataSourceFactory.getMutableLiveData();
+
+        PagedList.Config config = new PagedList.Config.Builder().setEnablePlaceholders(true)
+                                        .setInitialLoadSizeHint(10)
+                                        .setPageSize(20)
+                                        .setPrefetchDistance(3)
+                                        .build();
+        executor = Executors.newCachedThreadPool();
+
+        pagedListLiveData = new LivePagedListBuilder<Long, Result>(movieDataSourceFactory, config)
+                                .setFetchExecutor(executor)
+                                .build();
+
     }
 
     public LiveData<List<Result>> getAllMovieData(){
@@ -25,5 +55,9 @@ public class MainActivityViewModel extends AndroidViewModel {
 
 
         return movieRepository.getMutableLiveData();
+    }
+
+    public LiveData<PagedList<Result>> getPagedListLiveData() {
+        return pagedListLiveData;
     }
 }
